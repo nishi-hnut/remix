a88370d8a78ff08dfc803719ee40d341922239f2
nishi <nishi.hnut@gmail.com>
2024-10-01 18:28:26 +0900

    s

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -9,45 +9,178 @@ export const meta: MetaFunction = () => {
 
 export default function Index() {
   return (
-    <div className="flex h-screen items-center justify-center">
-      <div className="flex flex-col items-center gap-16">
-        <header className="flex flex-col items-center gap-9">
-          <h1 className="leading text-2xl font-bold text-gray-800 dark:text-gray-100">
-            Welcome to <span className="sr-only">Remix</span>
-          </h1>
-          <div className="h-[144px] w-[434px]">
-            <img
-              src="/logo-light.png"
-              alt="Remix"
-              className="block w-full dark:hidden"
-            />
-            <img
-              src="/logo-dark.png"
-              alt="Remix"
-              className="hidden w-full dark:block"
-            />
+    <div className="relative bg-black max-w-xs mx-auto px-2 py-2">
+      <header>
+        <div className="max-w-xs mx-auto text-center flex items-center justify-between">
+          <img className="w-4 h-4 object-cover" src="serch.png" alt="" />
+
+          <div className="flex items-center">
+            <img className="w-5 h-5 object-cover" src="p.png" alt="" />
+            <p className="text-white font-bold ml-1">310 +</p>
           </div>
-        </header>
-        <nav className="flex flex-col items-center justify-center gap-4 rounded-3xl border border-gray-200 p-6 dark:border-gray-700">
-          <p className="leading-6 text-gray-700 dark:text-gray-200">
-            What&apos;s next?
-          </p>
-          <ul>
-            {resources.map(({ href, text, icon }) => (
-              <li key={href}>
-                <a
-                  className="group flex items-center gap-3 self-stretch p-3 leading-normal text-blue-700 hover:underline dark:text-blue-500"
-                  href={href}
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  {icon}
-                  {text}
-                </a>
-              </li>
-            ))}
-          </ul>
-        </nav>
+
+          <img className="w-4 h-4 object-cover" src="time.png" alt="" />
+        </div>
+      </header>
+
+      <main className="max-w-xs mx-auto">
+        <div className="flex justify-between mt-4">
+          <p className="text-white text-xs font-bold">いますぐ通話可能なお相手</p>
+          <p className="text-purple-400 text-xs font-bold">更新する</p>
+        </div>
+
+        <div className="mt-1 flex">
+          <div className="text-center inline-block">
+            <img className="w-16 h-16 object-cover rounded-full pointer-events-none" src="neko.jpg" alt="" />
+            <p className="text-white">neko</p>
+          </div>
+
+          <div className="text-center inline-block ml-3 flex-none">
+            <img className="w-16 h-16 object-cover rounded-full pointer-events-none" src="neko.jpg" alt="" />
+            <p className="text-white">neko</p>
+          </div>
+
+          <div className="text-center inline-block ml-3 flex-none">
+            <img className="w-16 h-16 object-cover rounded-full pointer-events-none" src="neko.jpg" alt="" />
+            <p className="text-white">neko</p>
+          </div>
+
+          <div className="text-center inline-block ml-3 flex-none">
+            <img className="w-16 h-16 object-cover rounded-full pointer-events-none" src="neko.jpg" alt="" />
+            <p className="text-white">neko</p>
+          </div>
+        </div>
+
+        <div className="mt-2 grid grid-cols-2 gap-2">
+          <div className="w-full h-44 relative inline-block">
+            <img className="w-full h-44 object-cover rounded-lg" src="neko.jpg" alt="" />
+            <div className="absolute bottom-0 rounded-b-lg bg-gray-600 bg-opacity-50 w-full">
+              <div className="flex justify-between items-center p-1">
+                <p className="bg-green-500 bg-opacity-50 p-0.5 rounded-full text-xs text-white">オンライン</p>
+
+                <div className="flex">
+                  <img className="w-5 h-5 object-cover" src="call.png" alt="" />
+                  <img className="w-6 h-6 object-cover ml-1" src="video.png" alt="" />
+                </div>
+              </div>
+              <p className="text-white text-sm p-1">のん(24)</p>
+            </div>
+          </div>
+          <div className="w-full h-44 relative inline-block">
+            <img className="w-full h-44 object-cover rounded-lg" src="neko.jpg" alt="" />
+            <div className="absolute bottom-0 rounded-b-lg bg-gray-600 bg-opacity-50 w-full">
+              <div className="flex justify-between items-center p-1">
+                <p className="bg-green-500 bg-opacity-50 p-0.5 rounded-full text-xs text-white">オンライン</p>
+
+                <div className="flex">
+                  <img className="w-5 h-5 object-cover" src="call.png" alt="" />
+                  <img className="w-6 h-6 object-cover ml-1" src="video.png" alt="" />
+                </div>
+              </div>
+              <p className="text-white text-sm p-1">のん(24)</p>
+            </div>
+          </div>
+          <div className="w-full h-44 relative inline-block">
+            <img className="w-full h-44 object-cover rounded-lg" src="neko.jpg" alt="" />
+            <div className="absolute bottom-0 rounded-b-lg bg-gray-600 bg-opacity-50 w-full">
+              <div className="flex justify-between items-center p-1">
+                <p className="bg-green-500 bg-opacity-50 p-0.5 rounded-full text-xs text-white">オンライン</p>
+
+                <div className="flex">
+                  <img className="w-5 h-5 object-cover" src="call.png" alt="" />
+                  <img className="w-6 h-6 object-cover ml-1" src="video.png" alt="" />
+                </div>
+              </div>
+              <p className="text-white text-sm p-1">のん(24)</p>
+            </div>
+          </div>
+          <div className="w-full h-44 relative inline-block">
+            <img className="w-full h-44 object-cover rounded-lg" src="neko.jpg" alt="" />
+            <div className="absolute bottom-0 rounded-b-lg bg-gray-600 bg-opacity-50 w-full">
+              <div className="flex justify-between items-center p-1">
+                <p className="bg-green-500 bg-opacity-50 p-0.5 rounded-full text-xs text-white">オンライン</p>
+
+                <div className="flex">
+                  <img className="w-5 h-5 object-cover" src="call.png" alt="" />
+                  <img className="w-6 h-6 object-cover ml-1" src="video.png" alt="" />
+                </div>
+              </div>
+              <p className="text-white text-sm p-1">のん(24)</p>
+            </div>
+          </div>
+          <div className="w-full h-44 relative inline-block">
+            <img className="w-full h-44 object-cover rounded-lg" src="neko.jpg" alt="" />
+            <div className="absolute bottom-0 rounded-b-lg bg-gray-600 bg-opacity-50 w-full">
+              <div className="flex justify-between items-center p-1">
+                <p className="bg-green-500 bg-opacity-50 p-0.5 rounded-full text-xs text-white">オンライン</p>
+
+                <div className="flex">
+                  <img className="w-5 h-5 object-cover" src="call.png" alt="" />
+                  <img className="w-6 h-6 object-cover ml-1" src="video.png" alt="" />
+                </div>
+              </div>
+              <p className="text-white text-sm p-1">のん(24)</p>
+            </div>
+          </div>
+          <div className="w-full h-44 relative inline-block">
+            <img className="w-full h-44 object-cover rounded-lg" src="neko.jpg" alt="" />
+            <div className="absolute bottom-0 rounded-b-lg bg-gray-600 bg-opacity-50 w-full">
+              <div className="flex justify-between items-center p-1">
+                <p className="bg-green-500 bg-opacity-50 p-0.5 rounded-full text-xs text-white">オンライン</p>
+
+                <div className="flex">
+                  <img className="w-5 h-5 object-cover" src="call.png" alt="" />
+                  <img className="w-6 h-6 object-cover ml-1" src="video.png" alt="" />
+                </div>
+              </div>
+              <p className="text-white text-sm p-1">のん(24)</p>
+            </div>
+          </div>
+          <div className="w-full h-44 relative inline-block">
+            <img className="w-full h-44 object-cover rounded-lg" src="neko.jpg" alt="" />
+            <div className="absolute bottom-0 rounded-b-lg bg-gray-600 bg-opacity-50 w-full">
+              <div className="flex justify-between items-center p-1">
+                <p className="bg-green-500 bg-opacity-50 p-0.5 rounded-full text-xs text-white">オンライン</p>
+
+                <div className="flex">
+                  <img className="w-5 h-5 object-cover" src="call.png" alt="" />
+                  <img className="w-6 h-6 object-cover ml-1" src="video.png" alt="" />
+                </div>
+              </div>
+              <p className="text-white text-sm p-1">のん(24)</p>
+            </div>
+          </div>
+          <div className="w-full h-44 relative inline-block">
+            <img className="w-full h-44 object-cover rounded-lg" src="neko.jpg" alt="" />
+            <div className="absolute bottom-0 rounded-b-lg bg-gray-600 bg-opacity-50 w-full">
+              <div className="flex justify-between items-center p-1">
+                <p className="bg-green-500 bg-opacity-50 p-0.5 rounded-full text-xs text-white">オンライン</p>
+
+                <div className="flex">
+                  <img className="w-5 h-5 object-cover" src="call.png" alt="" />
+                  <img className="w-6 h-6 object-cover ml-1" src="video.png" alt="" />
+                </div>
+              </div>
+              <p className="text-white text-sm p-1">のん(24)</p>
+            </div>
+          </div>
+        </div>
+
+        <section className="hero is-primary">
+          <div className="hero-body">
+            <p className="title">Hello, Remix with Bulma!</p>
+          </div>
+        </section>
+      </main>
+
+      <div className="sticky max-w-xs mx-auto bottom-0 -translate-y-20 right-0 z-50 ml-52 justify-end">
+        <div className="bg-red-500 flex items-center py-2 px-1.5 w-20 rounded-full">
+          <p className="text-white text-xs flex-1 font-bold">今すぐ通話</p>
+        </div>
+
+        <div className="bg-purple-500 flex items-center py-2 px-1 w-20 rounded-full my-3">
+          <p className="text-white text-xs flex-1 font-bold">通話待機する</p>
+        </div>
       </div>
     </div>
   );
