c0ad27cb64ed40a651b10cc41c67afda45364b3d
nishi <nishi.hnut@gmail.com>
2024-10-02 15:11:07 +0900

    更新

--- app/routes/pointBoard.tsx
+++ app/routes/pointBoard.tsx
@@ -1,56 +1,56 @@
 export default function pointBoard() {
     return (
-        <div className="max-w-md mx-auto bg-black text-white">
+        <div className="max-w-md mx-auto bg-black text-white mb-20">
 
             <header className="text-center py-6 sticky">
                 <p className="font-bold">ポイント表</p>
             </header>
 
-            <div className="mt-10 bg-neutral-800">
+            <div className="mt-10 bg-neutral-900">
 
-                <div className="border-t border-b border-gray-300 border-opacity-20 py-7">
+                <div className="border-t border-b border-gray-300 border-opacity-20 py-5">
                     <div className="flex justify-between mx-4">
                         <p>メッセージ送信</p>
                         <p>60pt</p>
                     </div>
                 </div>
 
-                <div className="border-t border-b border-gray-300 border-opacity-20 py-7">
+                <div className="border-t border-b border-gray-300 border-opacity-20 py-5">
                     <div className="flex justify-between mx-4">
                         <p>スタンプ送信</p>
                         <p>60pt</p>
                     </div>
                 </div>
 
-                <div className="border-t border-b border-gray-300 border-opacity-20 py-7">
+                <div className="border-t border-b border-gray-300 border-opacity-20 py-5">
                     <div className="flex justify-between mx-4">
                         <p>画像付きメッセージ(開封・送信)</p>
                         <p>60pt</p>
                     </div>
                 </div>
 
-                <div className="border-t border-b border-gray-300 border-opacity-20 py-7">
+                <div className="border-t border-b border-gray-300 border-opacity-20 py-5">
                     <div className="flex justify-between mx-4">
                         <p>ボイス付きメッセージ(開封・送信)</p>
                         <p>60pt</p>
                     </div>
                 </div>
 
-                <div className="border-t border-b border-gray-300 border-opacity-20 py-7">
+                <div className="border-t border-b border-gray-300 border-opacity-20 py-5">
                     <div className="flex justify-between mx-4">
                         <p>動画付きメッセージ(開封・送信)</p>
                         <p>160pt</p>
                     </div>
                 </div>
 
-                <div className="border-t border-b border-gray-300 border-opacity-20 py-7">
+                <div className="border-t border-b border-gray-300 border-opacity-20 py-5">
                     <div className="flex justify-between mx-4">
                         <p>音声通話(1分)</p>
                         <p>160pt</p>
                     </div>
                 </div>
 
-                <div className="border-t border-b border-gray-300 border-opacity-20 py-7">
+                <div className="border-t border-b border-gray-300 border-opacity-20 py-5">
                     <div className="flex justify-between mx-4">
                         <p>ビデオ通話(1分)</p>
                         <p>280pt</p>
